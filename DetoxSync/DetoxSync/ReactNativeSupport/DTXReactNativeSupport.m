79d4296c9a9bf0357142e244af9d614d3e08fbde
Leo Natan <leo.natan@outlook.com>
2020-10-01 18:16:15 +0300

    Do not synchronize the JS display link used for timers

--- DetoxSync/DetoxSync/ReactNativeSupport/DTXReactNativeSupport.m
+++ DetoxSync/DetoxSync/ReactNativeSupport/DTXReactNativeSupport.m
@@ -182,14 +182,14 @@ static void _setupRNSupport()
 		__orig_loadBundleAtURL_onProgress_onComplete = (void*)method_getImplementation(m);
 		method_setImplementation(m, (void*)__detox_sync_loadBundleAtURL_onProgress_onComplete);
 		
-		cls = NSClassFromString(@"RCTDisplayLink");
-		m = class_getInstanceMethod(cls, @selector(init));
-		id(*orig_init_imp)(id, SEL) = (id(*)(id, SEL))method_getImplementation(m);
-		method_setImplementation(m, imp_implementationWithBlock(^id(id _self) {
-			id rv = orig_init_imp(_self, @selector(init));
-			[DTXSyncManager trackDisplayLink:[rv valueForKey:@"jsDisplayLink"]];
-			return rv;
-		}));
+//		cls = NSClassFromString(@"RCTDisplayLink");
+//		m = class_getInstanceMethod(cls, @selector(init));
+//		id(*orig_init_imp)(id, SEL) = (id(*)(id, SEL))method_getImplementation(m);
+//		method_setImplementation(m, imp_implementationWithBlock(^id(id _self) {
+//			id rv = orig_init_imp(_self, @selector(init));
+//			[DTXSyncManager trackDisplayLink:[rv valueForKey:@"jsDisplayLink"]];
+//			return rv;
+//		}));
 	}
 }
 
