30ccb1099a29b7b798a4e305941d6f438ce7d5ab
Asaf Korem <55082339+asafkorem@users.noreply.github.com>
2021-11-22 12:03:25 +0200

    NSArray+Functional: initial commit.

---
+++ DetoxSync/DetoxSync/Utils/NSArray+Functional.h
@@ -0,0 +1,28 @@
+//
+//  NSArray+Functional.h
+//  DetoxSync
+//
+//  Created by asaf korem on 22/11/2021.
+//  Copyright © 2021 wix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Category for functional methods on arrays.
+@interface NSArray (Functional)
+
+typedef BOOL(^FilterBlock)(id object);
+
+/// Filters objects in the array using the given \c block.
+- (NSArray *)filter:(FilterBlock)block;
+
+typedef id _Nonnull (^MapBlock)(id object);
+
+/// Maps objects in the array using the given \c block.
+- (NSArray *)map:(MapBlock)block;
+
+@end
+
+NS_ASSUME_NONNULL_END
