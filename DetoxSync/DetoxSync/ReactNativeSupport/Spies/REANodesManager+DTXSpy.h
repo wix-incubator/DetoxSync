5a812ba0eb2e05e14703fa02481f0c88c2b34b7c
Asaf Korem <55082339+asafkorem@users.noreply.github.com>
2023-05-03 16:41:43 +0300

    feat: synchronize with Reanimated animations. (#59)
    * feat: synchronize with Reanimated animations.
    
    * feat: synchronize with Reanimated (v2) animations.
    
    * Remove EL

---
+++ DetoxSync/DetoxSync/ReactNativeSupport/Spies/REANodesManager+DTXSpy.h
@@ -0,0 +1,14 @@
+//
+//  REANodesManager+DTXSpy.h (DetoxSync)
+//  Created by Asaf Korem (Wix.com) on 2023.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (REANodesManagerDTXSpy)
+
+@end
+
+NS_ASSUME_NONNULL_END
