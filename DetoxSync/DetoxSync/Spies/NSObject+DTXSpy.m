553308605d13367fe8564862ab2b55be6cbdd9a4
Asaf Korem <55082339+asafkorem@users.noreply.github.com>
2023-10-30 12:12:42 +0200

    NSObject+DTXSpy: ignore perform-selector sync for special case. (#69)
    * project: apply Xcode automatic changes.
    
    * NSObject+DTXSpy: ignore sync for special case.

--- DetoxSync/DetoxSync/Spies/NSObject+DTXSpy.m
+++ DetoxSync/DetoxSync/Spies/NSObject+DTXSpy.m
@@ -28,38 +28,53 @@
 
 + (void)load
 {
-	@autoreleasepool
-	{
-		NSError* error;
-		
-		DTXSwizzleMethod(self, @selector(performSelector:withObject:afterDelay:inModes:), @selector(__detox_sync_performSelector:withObject:afterDelay:inModes:), &error);
-		DTXSwizzleMethod(self, @selector(performSelector:onThread:withObject:waitUntilDone:modes:), @selector(__detox_sync_performSelector:onThread:withObject:waitUntilDone:modes:), &error);
-	}
+  @autoreleasepool
+  {
+    NSError* error;
+
+    DTXSwizzleMethod(self, @selector(performSelector:withObject:afterDelay:inModes:), @selector(__detox_sync_performSelector:withObject:afterDelay:inModes:), &error);
+    DTXSwizzleMethod(self, @selector(performSelector:onThread:withObject:waitUntilDone:modes:), @selector(__detox_sync_performSelector:onThread:withObject:waitUntilDone:modes:), &error);
+  }
 }
 
 
 - (void)__detox_sync_performSelector:(SEL)aSelector withObject:(id)anArgument afterDelay:(NSTimeInterval)delay inModes:(NSArray<NSRunLoopMode> *)modes
 {
-	if([DTXSyncManager isThreadTracked:NSThread.currentThread] == NO || delay > DTXSyncManager.maximumAllowedDelayedActionTrackingDuration)
-	{
-		[self __detox_sync_performSelector:aSelector withObject:anArgument afterDelay:delay inModes:modes];
-		return;
-	}
-	
-	id trampoline = [DTXDelayedPerformSelectorSyncResource delayedPerformSelectorProxyWithTarget:self selector:aSelector object:anArgument];
-	[trampoline __detox_sync_performSelector:@selector(fire) withObject:nil afterDelay:delay inModes:modes];
+  if([DTXSyncManager isThreadTracked:NSThread.currentThread] == NO ||
+     delay > DTXSyncManager.maximumAllowedDelayedActionTrackingDuration ||
+     [self shouldDisableTrackingForDelayedSelector:aSelector withTarget:self object:anArgument])
+  {
+    [self __detox_sync_performSelector:aSelector withObject:anArgument afterDelay:delay inModes:modes];
+    return;
+  }
+
+  id trampoline = [DTXDelayedPerformSelectorSyncResource delayedPerformSelectorProxyWithTarget:self selector:aSelector object:anArgument];
+  [trampoline __detox_sync_performSelector:@selector(fire) withObject:nil afterDelay:delay inModes:modes];
+}
+
+- (BOOL)shouldDisableTrackingForDelayedSelector:(SEL)aSelector withTarget:(id)target object:(id)obj
+{
+  // This is a workaround to ignore special edge case of delayed gesture recognizer cancelation
+  //  from react-native-gesture-handler.
+  if (target &&
+      [NSStringFromClass([target class]) isEqualToString:@"RNBetterTapGestureRecognizer"] &&
+      [NSStringFromSelector(aSelector) isEqualToString:@"cancel"]) {
+    return YES;
+  }
+
+  return NO;
 }
 
 - (void)__detox_sync_performSelector:(SEL)aSelector onThread:(NSThread *)thr withObject:(id)arg waitUntilDone:(BOOL)wait modes:(NSArray<NSString *> *)array
 {
-	if([DTXSyncManager isThreadTracked:thr] == NO)
-	{
-		[self __detox_sync_performSelector:aSelector onThread:thr withObject:arg waitUntilDone:wait modes:array];
-		return;
-	}
-	
-	id trampoline = [DTXDelayedPerformSelectorSyncResource delayedPerformSelectorProxyWithTarget:self selector:aSelector object:arg];
-	[trampoline __detox_sync_performSelector:@selector(fire) onThread:thr withObject:nil waitUntilDone:wait modes:array];
+  if([DTXSyncManager isThreadTracked:thr] == NO)
+  {
+    [self __detox_sync_performSelector:aSelector onThread:thr withObject:arg waitUntilDone:wait modes:array];
+    return;
+  }
+
+  id trampoline = [DTXDelayedPerformSelectorSyncResource delayedPerformSelectorProxyWithTarget:self selector:aSelector object:arg];
+  [trampoline __detox_sync_performSelector:@selector(fire) onThread:thr withObject:nil waitUntilDone:wait modes:array];
 }
 
 @end
